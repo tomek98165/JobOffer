14761d513acd6476d138983b026728ba29a3c821
Tomek <tomek122145@gmail.com>
2023-10-26 22:04:20 +0200

    Add Integration test and add Spring boot, Wiremock, Test Containers and MongoDB

---
+++ src/main/java/com/joboffers/JobOffersSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.joboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersSpringBootApplication.class, args);
+    }
+}
